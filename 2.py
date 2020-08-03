24b3115b1a45fe91e3b4766fe86540442b1a6b72
hasiniranasinghe <61630657+hasiniranasinghe@users.noreply.github.com>
2020-08-03 18:44:29 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -15,14 +15,22 @@ from keras.datasets import mnist
 plt.imshow(X_train[0])
 plt.show()
 
-X_train = X_train.reshape(X_train.shape[0],28, 28, 1) 
-X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+# add noice eith noice factor 0.2
+noise_factor = 0.2
+x_train_noisy = train_images + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=train_images.shape) 
+x_test_noisy = test_images + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=test_images.shape) 
 
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
 
-X_train /= 255
-X_test /= 255
+x_train_noisy = x_train_noisy.reshape(x_train_noisy.shape[0],28, 28, 1) 
+x_test_noisy = x_test_noisy.reshape(x_test_noisy.shape[0], 28, 28, 1)
+
+x_train_noisy = x_train_noisy.astype('float32')
+x_test_noisy = x_test_noisy.astype('float32')
+
+x_train_noisy /= 255
+x_test_noisy /= 255
 
 Y_train = np_utils.to_categorical(y_train, 10) 
 Y_test = np_utils.to_categorical(y_test, 10)
@@ -42,7 +50,7 @@ model.add(Dense(10, activation='softmax'))
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
 
-model.fit(X_train, Y_train, batch_size=32, nb_epoch=1, verbose=1)
+model.fit(x_train_noisy, Y_train, batch_size=32, nb_epoch=1, verbose=1)
            
  
 for i in np.random.choice(np.arange(0, len(Y_test)), size = (10,)):
@@ -50,7 +58,7 @@ for i in np.random.choice(np.arange(0, len(Y_test)), size = (10,)):
 	probs = model.predict(X_test[np.newaxis, i])
 	prediction = probs.argmax(axis=1)
  
-	image = (X_test[i] * 255).reshape((28, 28)).astype("uint8")
+	image = (x_test_noisy[i] * 255).reshape((28, 28)).astype("uint8")
  
 	print "Actual digit is {0}, predicted {1}".format(Y_test[i], prediction[0])
 	cv2.imshow("Digit", image)
